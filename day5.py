3c7616d92bbd9e99506f896e7e76d81e2a72cbfb
Nathan Self <nwself@cs.vt.edu>
2021-12-05 15:27:20 -0500

    Refactor to namedtuple

--- day5.py
+++ day5.py
@@ -1,15 +1,17 @@
 import collections
 import itertools
 
+Point = collections.namedtuple("Point", ["x", "y"])
+
 print(len([v for v in collections.Counter(itertools.chain.from_iterable([
     [
         (
-            int(line[0][0] - i * (line[0][0] - line[1][0]) / abs(line[0][0] - line[1][0]) if line[0][0] != line[1][0] else line[0][0]),
-            int(line[0][1] - i * (line[0][1] - line[1][1]) / abs(line[0][1] - line[1][1]) if line[0][1] != line[1][1] else line[0][1])
-        ) for i in range(max([abs(line[0][0] - line[1][0]), abs(line[0][1] - line[1][1])]) + 1)
+            int(line[0].x - i * (line[0].x - line[1].x) / abs(line[0].x - line[1].x) if line[0].x != line[1].x else line[0].x),
+            int(line[0].y - i * (line[0].y - line[1].y) / abs(line[0].y - line[1].y) if line[0].y != line[1].y else line[0].y)
+        ) for i in range(max([abs(line[0].x - line[1].x), abs(line[0].y - line[1].y)]) + 1)
     ]
     for line in map(
-        lambda line: [tuple(map(int, c.split(","))) for c in line],
+        lambda line: [Point(*map(int, c.split(","))) for c in line],
         (line.strip().split(" -> ") for line in open("./input5.txt"))
     )
 ])).values() if v >= 2]))
