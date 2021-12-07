2c1bca23dc8c047e04ba9e307fe75d2f0981a6b6
Nathan Self <nwself@cs.vt.edu>
2021-12-07 09:18:13 -0500

    Day 7

---
+++ day7.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+crabs = list(map(int, next(open("./input7.txt")).split(",")))
+
+print(min([
+    sum([
+        (abs(crab - winner) * (abs(crab - winner) + 1)) / 2 for crab in crabs
+    ]) for winner in range(0, max(crabs))
+]))
